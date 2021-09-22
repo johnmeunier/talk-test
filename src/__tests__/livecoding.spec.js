46795eb44987d427021602ec11c33dc77b63684d
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2021-09-22 15:20:00 +0200

    livecoding2

--- src/__tests__/livecoding.spec.js
+++ src/__tests__/livecoding.spec.js
@@ -12,4 +12,23 @@ describe("insert new items in the todo app", () => {
       expect(screen.getByText(/Groceries/i)).toBeInTheDocument()
     );
   });
+
+  test("inserting multiple items into the todo app", async () => {
+    render(<App />);
+    const input = screen.getByLabelText(/Add/i);
+    userEvent.type(input, "Groceries");
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+
+    userEvent.type(input, "Pick up the girls from school");
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+
+    await waitFor(() =>
+      expect(screen.getByText(/Groceries/i)).toBeInTheDocument()
+    );
+    await waitFor(() =>
+      expect(
+        screen.getByText(/Pick up the girls from school/i)
+      ).toBeInTheDocument()
+    );
+  });
 });
