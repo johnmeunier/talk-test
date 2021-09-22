8c34e33ff2a5d03dfa3a30902173d255e98cef89
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2021-09-22 15:50:37 +0200

    livecoding3

--- src/__tests__/livecoding.spec.js
+++ src/__tests__/livecoding.spec.js
@@ -1,6 +1,13 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { defineFeature, loadFeature } from "jest-cucumber";
+
 import App from "../App";
+
+const feature = loadFeature("./livecoding.feature", {
+  loadRelativePath: true,
+});
+
 describe("insert new items in the todo app", () => {
   test("insert 1 item", async () => {
     render(<App />);
@@ -32,3 +39,56 @@ describe("insert new items in the todo app", () => {
     );
   });
 });
+
+defineFeature(feature, (test) => {
+  test("Adding a single item", ({ given, when, then, and }) => {
+    givenIamOnTheTodoApp(given);
+    whenIWriteInTheNewItem(when);
+    whenIAddMyNewItem(and);
+    then(/My todo has the item "(.*)"/, async (itemName) => {
+      await waitFor(() =>
+        expect(screen.getByText(itemName)).toBeInTheDocument()
+      );
+    });
+  });
+
+  test("Add multiple items to the list", ({ given, when, then, and }) => {
+    givenIamOnTheTodoApp(given);
+    whenIWriteInTheNewItem(when);
+    whenIAddMyNewItem(and);
+    whenIWriteInTheNewItem(when);
+    whenIAddMyNewItem(and);
+    whenIWriteInTheNewItem(when);
+    whenIAddMyNewItem(and);
+    then("My Todo-list has the items :", async (table) => {
+      const expectedItems = table.map((r) => r.items);
+      await waitFor(() =>
+        expect(
+          screen.getAllByRole("listitem")[expectedItems.length - 1]
+        ).toBeInTheDocument()
+      );
+      const items = screen.getAllByRole("listitem");
+
+      expectedItems.forEach((item, index) => {
+        expect(items[index]).toHaveTextContent(item);
+      });
+    });
+  });
+});
+function whenIAddMyNewItem(and) {
+  and(/I click on the add an item button/, () => {
+    userEvent.click(screen.getByRole("button", { name: "+" }));
+  });
+}
+
+function whenIWriteInTheNewItem(when) {
+  when(/I write "(.*)" in the new item/, (typedText) => {
+    userEvent.type(screen.getByLabelText(/Add/), typedText);
+  });
+}
+
+function givenIamOnTheTodoApp(given) {
+  given(/I am on the todo app/, () => {
+    render(<App />);
+  });
+}
